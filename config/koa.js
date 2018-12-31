32c0291ab296ff3c5a04eb15f5e1182cf186b6ac
Emile Fugulin <emilefugulin@hotmail.com>
2018-12-30 22:39:05 -0500

    [FIX] Relax cors and and better error handling

--- config/koa.js
+++ config/koa.js
@@ -1,9 +1,10 @@
+const _ = require('lodash');
 const cors = require('kcors');
 const session = require('koa-session');
 const responseTime = require('koa-response-time');
 const logger = require('koa-logger');
 const compress = require('koa-compress');
-const errorHandler = require('koa-error');
+const errorHandler = require('koa-json-error');
 const bodyParser = require('koa-bodyparser');
 
 const config = require('./config');
@@ -15,13 +16,17 @@ module.exports = function (app, passport) {
   app.proxy = config.app.proxy || false;
   if (config.app.env === 'dev') {
     app.use(logger());
-    app.use(cors());
+    app.use(cors({
+      credentials: true
+    }));
   }
 
+  // JSON error handler
   app.use(errorHandler({
-    template: config.app.root + '/src/views/error.html'
+    postFormat: (e, obj) => process.env.NODE_ENV === 'prod' ? _.omit(obj, 'stack') : obj
   }));
 
+  // Session management
   app.use(session(app));
   app.use(bodyParser());
   app.use(passport.initialize());
